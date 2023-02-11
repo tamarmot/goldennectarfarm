df181e0579e94fea2951bc0e06b82f811040a3a2
Tamar Cohen <tamar.e.cohen@nasa.gov>
2023-02-10 18:28:02 -0800

    wip theme

--- react_mui/create-react-app/src/theme.js
+++ react_mui/create-react-app/src/theme.js
@@ -1,19 +1,45 @@
-import { red } from '@mui/material/colors';
 import { createTheme } from '@mui/material/styles';
 
 // A custom theme for this app
 const theme = createTheme({
   palette: {
+    mode: 'light',
     primary: {
-      main: '#556cd6',
+      main: '#66603b',
     },
     secondary: {
-      main: '#19857b',
+      main: '#f50057',
     },
-    error: {
-      main: red.A400,
+    background: {
+      default: '#c7be8d',
+//      paper: '#e7dead',
+      paper: '#c7be8d',
     },
   },
+  typography: {
+    fontFamily: [
+      'Droid Serif',
+      'Georgia'
+    ].join(','),
+  }
 });
+
+// import { ThemeOptions } from '@mui/material/styles';
+
+// export const themeOptions: ThemeOptions = {
+//   palette: {
+//     mode: 'light',
+//     primary: {
+//       main: '#66603b',
+//     },
+//     secondary: {
+//       main: '#f50057',
+//     },
+//     background: {
+//       default: '#c7be8d',
+//       paper: '#e7dead',
+//     },
+//   },
+// };
 
 export default theme;
